9c9d8b1af9367a3445bb187a7b6cc369d7d51109
Lilithe Bowman <lilithe.bowman@gmail.com>
2025-02-15 21:39:33 -0500

    first commit

---
+++ Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	private CharacterController controller;
+	private Vector3 playerVelocity;
+	private bool groundedPlayer;
+	private float playerSpeed = 2.0f;
+	private float jumpHeight = 1.0f;
+	private float gravityValue = -9.81f;
+
+	private void Start () {
+		controller = gameObject.GetComponent<CharacterController>();
+		if (controller == null) {
+			controller = gameObject.AddComponent<CharacterController>();
+		}
+		Debug.Log("Controller initialized to:");
+		Debug.Log(controller);
+	}
+
+	void Update () {
+		if (controller == null) {
+			Debug.Log("CharacterController not set... why?!??!?");
+			return;
+		}
+		groundedPlayer = controller.isGrounded;
+		if (groundedPlayer && playerVelocity.y < 0) {
+			playerVelocity.y = 0f;
+		}
+
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		controller.Move(move * Time.deltaTime * playerSpeed);
+
+		if (move != Vector3.zero) {
+			gameObject.transform.forward = move;
+		}
+
+		// Makes the player jump
+		if (Input.GetButtonDown("Jump") && groundedPlayer) {
+			playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+		}
+
+		playerVelocity.y += gravityValue * Time.deltaTime;
+		controller.Move(playerVelocity * Time.deltaTime);
+	}
+}
